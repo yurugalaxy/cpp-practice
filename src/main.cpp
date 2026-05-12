9f19f022f81ed9347e69d76a5f12dc8f7d54353c
yurugalaxy <code@kae.dev>
2026-05-12 07:27:19 +0100

    add random

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "random.h"
+
 void clearScreen() {
 	//Clear for Linux / Mac
 #ifdef linux
@@ -12,11 +14,35 @@ void clearScreen() {
 #endif
 }
 
+int convertInput() {
+
+	char userChar { };
+	std::cin >> userChar;
+
+	switch (userChar)
+	{
+		case 'r':
+			return 0;
+		case 'p':
+			return 1;
+		case 's':
+			return 2;
+		default:
+			std::cout << "Not recognised, exiting." << '\n';
+			return -1;
+	}
+}
 
 int main() {
 
 	clearScreen();
 	std::cout << "Hewo" << '\n';
+	std::cout << "Choose (r)ock, (p)aper, or (s)cissors:" << '\n';
+
+	int checky {convertInput()};
+
+	std::cout << Random::get(1,4) << '\n';
+	std::cout << checky << '\n';
 
 	return 0;
 }
